728f12914771154251e53edb65a506032ae2b9a4
austinwoo123 <austinwoo123@gmail.com>
2021-02-26 15:26:58 -0800

    edit table renering

--- src/components/DataArea/index.js
+++ src/components/DataArea/index.js
@@ -1,31 +1,34 @@
 import React, { Component } from "react";
-
+import Employees from "../../Data/employees.json";
 export default class DataArea extends Component {
     state = {
-        employees: [],
-        allEmployees: [],
-        isLoading: true,
+        employees: Employees,
+        allEmployees: Employees,
+        isLoading: false,
     };
 
     headings = ["name..."];
 
 
-    componentDidMount() {
-        fetch("/employees.json")
-            .then(function (response) {
-                return response.json();
-            })
-            .then((response) => {
-                this.setState({
-                    employees: response,
-                    allEmployees: response,
-                    isLoading: false,
-                });
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    }
+    // componentDidMount() {
+    //     console.log("hello")
+    //     fetch("/employees.json")
+    //         .then(function (response) {
+    //             console.log(response)
+    //             return response.json();
+    //         })
+    //         .then((response) => {
+    //             console.log(response);
+    //             this.setState({
+    //                 employees: response,
+    //                 allEmployees: response,
+    //                 isLoading: false,
+    //             });
+    //         })
+    //         .catch((error) => {
+    //             console.log(error);
+    //         });
+    // }
 
     sortDob = (event) => {
         const dob = event.target.value;
@@ -34,7 +37,7 @@ export default class DataArea extends Component {
                 employees: this.state.employees.sort(function (x, y) {
                     var date1 = new Date(x.dob).getTime();
                     var date2 = new Date(y.dob).getTime();
-                    return date1 > date2 ? 1 : -1;
+                    return (date1 > date2) ? 1 : -1;
                 }),
             });
         } else if (dob === "young") {
@@ -42,7 +45,7 @@ export default class DataArea extends Component {
                 employees: this.state.employees.sort(function (x, y) {
                     var date1 = new Date(x.dob).getTime();
                     var date2 = new Date(y.dob).getTime();
-                    return date1 < date2 ? 1 : -1;
+                    return (date1 < date2) ? 1 : -1;
                 }),
             });
         }
@@ -60,13 +63,15 @@ export default class DataArea extends Component {
                     </select>
                 </div>
                 <table className="table table-striped">
-                    <tr>
-                        <th>Picture</th>
-                        <th>Name</th>
-                        <th>Date of Birth</th>
-                        <th>Email</th>
-                        <th>Phone</th>
-                    </tr>
+                    <thead>
+                        <tr>
+                            <th>Picture</th>
+                            <th>Name</th>
+                            <th>Date of Birth</th>
+                            <th>Email</th>
+                            <th>Phone</th>
+                        </tr>
+                    </thead>
                     <tbody>
                         {this.state.employees.map(function (props) {
                             console.log(props.name)
