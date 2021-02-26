07ec80d205d06d8f908c83bbc9d5ff44a043ffae
austinwoo123 <austinwoo123@gmail.com>
2021-02-25 17:28:59 -0800

    get table to render and fix problem with one of the pictures not loading

--- src/components/DataArea/index.js
+++ src/components/DataArea/index.js
@@ -12,22 +12,61 @@ export default class DataArea extends Component {
 
     componentDidMount() {
         fetch("/employees.json")
-            .then(function (res) {
-                return res.json();
+            .then(function (response) {
+                return response.json();
             })
-            .then((res) => {
+            .then((response) => {
                 this.setState({
-                    employees: res,
-                    allEmployees: res,
+                    employees: response,
+                    allEmployees: response,
                     isLoading: false,
                 });
             })
-            .catch((err) => {
-                console.log(err);
-            })
+            .catch((error) => {
+                console.log(error);
+            });
     }
-    return(
 
-    )
+    render() {
+        return (
+            <div>
+                <table className="table table-striped">
 
+                    <tr>
+                        <th>Picture</th>
+                        <th>Name</th>
+                        <th>Date of Birth</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                    </tr>
+
+                    <tbody>
+                        {this.state.employees.map(function (props) {
+                            return (
+                                <tr key={props.id}>
+                                    <td>
+                                        <img alt="pic" src={props.pic}></img>
+                                    </td>
+                                    <td>{props.name}</td>
+                                    <td>{props.dob}</td>
+                                    <td>{props.email}</td>
+                                    <td>{props.phone}</td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
 }
+
+
+
+
+
+
+
+
+
+
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,7 +5,7 @@ function Header() {
     return (
         <div className="header" >
             <h1>Employee Directory</h1>
-            <p> </p>
+            <p>See information about your employees here </p>
         </div>
     )
 }
